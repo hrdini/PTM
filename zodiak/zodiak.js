4fa889ba252a52e2a7bc1b6f8e8742d2deae2edb
hrdini <87311745+hrdini@users.noreply.github.com>
2022-02-04 20:55:19 +0700

    Add files via upload

---
+++ zodiak/zodiak.js
@@ -0,0 +1,124 @@
+zodiak.onclick = function () {
+  let bulan = document.querySelector("#bulan").value;
+  let tanggal = document.querySelector("#tanggal").value;
+  let zodiak = "";
+  if (bulan == 3 && tanggal >= 21 && tanggal <= 31) {
+    zodiak = "ARIES";
+  }
+  document.querySelector("#hasil").innerHTML = zodiak;
+
+  if (bulan == 4 && tanggal >= 1 && tanggal <= 19) {
+    zodiak = "ARIES";
+  }
+  document.querySelector("#hasil").innerHTML = zodiak;
+
+  if (bulan == 4 && tanggal >= 20 && tanggal <= 30) {
+    zodiak = "TAURUS";
+  }
+  document.querySelector("#hasil").innerHTML = zodiak;
+
+  if (bulan == 5 && tanggal >= 1 && tanggal <= 20) {
+    zodiak = "TAURUS";
+  }
+  document.querySelector("#hasil").innerHTML = zodiak;
+
+  if (bulan == 5 && tanggal >= 21 && tanggal <= 31) {
+    zodiak = "GEMINI";
+  }
+  document.querySelector("#hasil").innerHTML = zodiak;
+
+  if (bulan == 6 && tanggal >= 1 && tanggal <= 20) {
+    zodiak = "GEMINI";
+  }
+  document.querySelector("#hasil").innerHTML = zodiak;
+
+  if (bulan == 6 && tanggal >= 21 && tanggal <= 30) {
+    zodiak = "CANCER";
+  }
+  document.querySelector("#hasil").innerHTML = zodiak;
+
+  if (bulan == 7 && tanggal >= 1 && tanggal <= 22) {
+    zodiak = "CANCER";
+  }
+  document.querySelector("#hasil").innerHTML = zodiak;
+
+  if (bulan == 7 && tanggal >= 23 && tanggal <= 31) {
+    zodiak = "LEO";
+  }
+  document.querySelector("#hasil").innerHTML = zodiak;
+
+  if (bulan == 8 && tanggal >= 1 && tanggal <= 22) {
+    zodiak = "LEO";
+  }
+  document.querySelector("#hasil").innerHTML = zodiak;
+
+  if (bulan == 8 && tanggal >= 23 && tanggal <= 31) {
+    zodiak = "VIRGO";
+  }
+  document.querySelector("#hasil").innerHTML = zodiak;
+
+  if (bulan == 9 && tanggal >= 1 && tanggal <= 22) {
+    zodiak = "VIRGO";
+  }
+  document.querySelector("#hasil").innerHTML = zodiak;
+
+  if (bulan == 9 && tanggal >= 23 && tanggal <= 30) {
+    zodiak = "LIBRA";
+  }
+  document.querySelector("#hasil").innerHTML = zodiak;
+
+  if (bulan == 10 && tanggal >= 1 && tanggal <= 22) {
+    zodiak = "LIBRA";
+  }
+  document.querySelector("#hasil").innerHTML = zodiak;
+
+  if (bulan == 10 && tanggal >= 23 && tanggal <= 31) {
+    zodiak = "SCORPIO";
+  }
+  document.querySelector("#hasil").innerHTML = zodiak;
+
+  if (bulan == 11 && tanggal >= 1 && tanggal <= 21) {
+    zodiak = "SCORPIO";
+  }
+  document.querySelector("#hasil").innerHTML = zodiak;
+
+  if (bulan == 11 && tanggal >= 22 && tanggal <= 30) {
+    zodiak = "SAGITARIUS";
+  }
+  document.querySelector("#hasil").innerHTML = zodiak;
+
+  if (bulan == 12 && tanggal >= 1 && tanggal <= 21) {
+    zodiak = "SAGITARIUS";
+  }
+  document.querySelector("#hasil").innerHTML = zodiak;
+
+  if (bulan == 12 && tanggal >= 22 && tanggal <= 31) {
+    zodiak = "CAPRICORN";
+  }
+  document.querySelector("#hasil").innerHTML = zodiak;
+
+  if (bulan == 1 && tanggal >= 1 && tanggal <= 19) {
+    zodiak = "CAPRICORN";
+  }
+  document.querySelector("#hasil").innerHTML = zodiak;
+
+  if (bulan == 1 && tanggal >= 20 && tanggal <= 31) {
+    zodiak = "AQUARIUS";
+  }
+  document.querySelector("#hasil").innerHTML = zodiak;
+
+  if (bulan == 2 && tanggal >= 1 && tanggal <= 18) {
+    zodiak = "AQUARIUS";
+  }
+  document.querySelector("#hasil").innerHTML = zodiak;
+
+  if (bulan == 2 && tanggal >= 19 && tanggal <= 28) {
+    zodiak = "PISCES";
+  }
+  document.querySelector("#hasil").innerHTML = zodiak;
+
+  if (bulan == 3 && tanggal >= 1 && tanggal <= 20) {
+    zodiak = "PISCES";
+  }
+  document.querySelector("#hasil").innerHTML = zodiak;
+};
